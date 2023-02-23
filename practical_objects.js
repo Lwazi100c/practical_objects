62d28227324695ad6ccf71c3ad67275c3116dd5b
Lwazi100c <sabelolwazi975@gmail.com>
2023-02-23 13:23:01 +0200

    The shoppingList function

---
+++ practical_objects.js
@@ -0,0 +1,14 @@
+var printShoppingList = function(list){
+    for(let k = 0; k < list.length; k++){
+        var currentItem = list[k].itemName;
+        var currentPrice = list[k].price;
+        console.log(`* ${currentItem} @ R${currentPrice}`)
+    }
+}
+
+var shoppingList = [
+    { itemName : 'Bread', price : 11.00 },
+    { itemName : 'Milk', price : 7.00 },
+    { itemName : 'Cheese', price : 19.50 }
+];
+printShoppingList(shoppingList)
