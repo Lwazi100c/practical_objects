4abf96d2e2ba0edba7decf1c3dc7df134e767ef0
Lwazi100c <sabelolwazi975@gmail.com>
2023-02-23 14:14:46 +0200

    "ShoppingList function totals"

--- practical_objects.js
+++ practical_objects.js
@@ -14,3 +14,32 @@ var shoppingList = [
 printShoppingList(shoppingList)
 
 
+
+var shoppingList = function(shoppingArray){
+    var totalPrice = 0;
+    for(let m = 0; m < shoppingArray.length; m++){
+        var currentPrice = shoppingArray[m].price;
+        totalPrice += currentPrice;
+    }
+    console.log(totalPrice);
+}
+//checking the function using the first data
+
+var shoppingList1 = [
+    { itemName : 'Bread', price : 11.00 },
+    { itemName : 'Milk', price : 7.00 },
+    { itemName : 'Cheese', price : 19.50 }
+];
+shoppingList(shoppingList1)//37.5
+
+//checking the function using the second data
+
+var shoppingList2 = [
+    { itemName : 'Apples', price : 7.50 },
+    { itemName : 'Bread', price : 11.00 },
+    { itemName : 'Milk', price : 7.00 },
+    { itemName : 'Cheese', price : 23.00 },
+    { itemName : 'Bread', price : 9.50 }
+];
+shoppingList(shoppingList2);
+
